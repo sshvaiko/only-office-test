e28d962a6cfb5872527425136738dda97176ba7a
Stan Shvaika <stan@shvaika.com>
2025-05-18 13:42:45 +0200

    Update pluginCode.js

--- pluginCode.js
+++ pluginCode.js
@@ -2,40 +2,27 @@
 ((window) => {
   const text = "Hello world!"
   window.Asc.plugin.init = () => {
-    const variant = 2
-
-    switch (variant) {
-    case 0:
-      // serialize command as text
-      let sScript = "var oDocument = Api.GetDocument();"
-      sScript += "oParagraph = Api.CreateParagraph();"
-      sScript += "oParagraph.AddText('Hello world!');"
-      sScript += "oDocument.InsertContent([oParagraph]);"
-      this.info.recalculate = true
-      this.executeCommand("close", sScript)
-      break
-    case 1:
-      // call command without external variables
-      this.callCommand(() => {
-        const oDocument = Api.GetDocument()
-        const oParagraph = Api.CreateParagraph()
-        oParagraph.AddText("Hello world!")
-        oDocument.InsertContent([oParagraph])
-      }, true)
-      break
-    case 2:
-      // call command with external variables
-      Asc.scope.text = text // export variable to plugin scope
-      this.callCommand(() => {
-        const oDocument = Api.GetDocument()
-        const oParagraph = Api.CreateParagraph()
-        oParagraph.AddText(Asc.scope.text) // or oParagraph.AddText(scope.text);
-        oDocument.InsertContent([oParagraph])
-      }, true)
-      break
-    default:
-      break
-    }
+    window.Asc.plugin.executeMethod("AddToolbarMenuItem", [{
+          guid: window.Asc.plugin.guid,
+          tabs: [{
+            id: "greenometer_tab",
+            text: "Greenometer",
+            items: [{
+              id: "greenometer_button",
+              type: "button",
+              text: "Insert CO2 Data",
+              hint: "Insert Greenometer message",
+              icons: "resources/icon.png"
+            }, 
+                   {
+              id: "greenometer_button",
+              type: "button",
+              text: "Insert ESRS Data",
+              hint: "Insert Greenometer message",
+              icons: "resources/icon.png"
+            }]
+          }]
+        }])
   }
 
   window.Asc.plugin.button = (id) => {}
