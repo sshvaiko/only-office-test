f03c35415a5124e34627129103c3d55723b98a6d
Stan Shvaika <stan@shvaika.com>
2025-05-21 13:51:05 +0200

    Update pluginCode.js

--- pluginCode.js
+++ pluginCode.js
@@ -1,42 +1,35 @@
-// Example insert text into editors (different implementations)
 ((window) => {
-  const text = "Hello world!"
   window.Asc.plugin.init = () => {
     window.Asc.plugin.executeMethod("AddToolbarMenuItem", [{
-          guid: window.Asc.plugin.guid,
-          tabs: [
-            
-                   {
-              id: "greenometer_button_esrs",
-              type: "button",
-              text: "Insert ESRS Data",
-              hint: "Insert Greenometer message",
-              icons: "resources/icon.png"
-            }]
-          }]
-        }])
+      guid: window.Asc.plugin.guid,
+      tabs: [{
+        id: "greenometer_tab",
+        text: "Greenometer",
+        items: [{
+          id: "greenometer_button_esrs",
+          type: "button",
+          text: "Insert ESRS Data",
+          hint: "Insert Greenometer message",
+          icons: "resources/icon.png"
+        }]
+      }]
+    }]);
 
     window.Asc.plugin.attachToolbarMenuClickEvent("greenometer_button_esrs", (data) => {
-      const message = {
-        source: "onlyoffice-plugin",
-        type: "greenometer_button_co2",
-        payload: data
-      };
-      
-      window.Asc.plugin.executeMethod("ShowError", ["Select data", 0]);
-      const randomId = Math.floor(Math.random() * 1_0000_0000); // Up to 8 digits
+      const randomId = Math.floor(Math.random() * 1_0000_0000);
       const text = `{{QUESTION_${randomId}}}`;
+
       window.Asc.plugin.executeMethod("InsertText", [text]);
 
-      window.Asc.plugin.executeMethod("StartAction", ["Block", "Save to local storage..."], function () {
-          setTimeout(function () {
-              window.Asc.plugin.executeMethod("EndAction", ["Block", "Save to local storage..."]);
-          }, 200);
+      window.Asc.plugin.executeMethod("StartAction", ["Block", "Insert ESRS Data..."], function () {
+        setTimeout(function () {
+          window.Asc.plugin.executeMethod("EndAction", ["Block", "Insert ESRS Data..."]);
+        }, 200);
       });
-      
-      console.log("greenometer_button_co2", data)
-    })
-  }
 
-  window.Asc.plugin.button = (id) => {}
-})(window)
+      console.log("Inserted:", text);
+    });
+  };
+
+  window.Asc.plugin.button = (id) => {};
+})(window);
